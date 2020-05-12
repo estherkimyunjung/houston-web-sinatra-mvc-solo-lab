609525a605d7cc8f947fbb7870da9fd768db7a79
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-12 17:54:21 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,36 +7,4 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :welcome
   end
-
-  get '/puppies' do
-    @puppies = Puppy.all
-    erb :index
-  end
-
-  get '/puppies/new' do
-    erb :new
-  end
-
-  post '/puppies' do
-    puppy = Puppy.create(params)
-    redirect "/puppies"
-  end
-
-  get '/puppies/:id' do 
-    @puppy = Puppy.find(params[:id]) 
-    erb :show
-  end
-
-  patch '/puppies/:id' do
-    puppy = Puppy.find(params[:id])
-    puppy.update(name: params[:name], breed: params[:breed])
-    redirect "/puppies"
-  end
-
-  delete '/puppies/:id' do
-    puppy = Puppy.find(params[:id])
-    puppy.destroy
-    redirect "/puppies"
-  end
-
 end
--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,2 +1,34 @@
 class PuppiesController < ApplicationController
+    
+    get '/puppies' do
+        @puppies = Puppy.all
+        erb :index
+    end
+    
+    get '/puppies/new' do
+        erb :new
+    end
+    
+    post '/puppies' do
+        puppy = Puppy.create(params)
+        redirect "/puppies"
+    end
+    
+    get '/puppies/:id' do 
+        @puppy = Puppy.find(params[:id]) 
+        erb :show
+    end
+    
+    patch '/puppies/:id' do
+        puppy = Puppy.find(params[:id])
+        puppy.update(name: params[:name], breed: params[:breed])
+        redirect "/puppies"
+    end
+    
+    delete '/puppies/:id' do
+        puppy = Puppy.find(params[:id])
+        puppy.destroy
+        redirect "/puppies"
+    end
+    
 end
