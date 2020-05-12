8f944b3b7bfcad1c86af8640edba208ddda06f70
estherkimyunjung <estherkimyunjung@gmail.com>
2020-05-12 15:35:46 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,42 @@
 class ApplicationController < Sinatra::Base
-    configure do
-        set :views, 'app/views'
-        set :method_override, true
-      end
+  configure do
+    set :views, 'app/views'
+    set :method_override, true
+  end
     
-      get '/' do
-        erb :welcome
-      end
+  get '/' do
+    erb :welcome
+  end
 
-   
+  get '/puppies' do
+    @puppies = Puppy.all
+    erb :index
+  end
+
+  get '/puppies/new' do
+    erb :new
+  end
+
+  post '/puppies' do
+    puppy = Puppy.create(params)
+    redirect "/puppies"
+  end
+
+  get '/puppies/:id' do 
+    @puppy = Puppy.find(params[:id]) 
+    erb :show
+  end
+
+  patch '/puppies/:id' do
+    puppy = Puppy.find(params[:id])
+    puppy.update(name: params[:name], breed: params[:breed])
+    redirect "/puppies"
+  end
+
+  delete '/puppies/:id' do
+    puppy = Puppy.find(params[:id])
+    puppy.destroy
+    redirect "/puppies"
+  end
+
 end
--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -1,3 +1,2 @@
 class PuppiesController < ApplicationController
-   
 end
